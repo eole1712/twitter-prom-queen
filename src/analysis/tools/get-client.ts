a5b9251c749b3ad64545692cc49a82f381213d84
eole1712 <grishjan@gmail.com>
2020-10-24 23:51:31 +0200

    fix: auto-creator

--- src/analysis/tools/get-client.ts
+++ src/analysis/tools/get-client.ts
@@ -1,3 +1,9 @@
 import Twitter from 'twitter-lite';
+import fs from 'fs';
+import path from 'path';
+
+if (!fs.existsSync(path.join(__dirname, `../../../data`))) {
+  fs.mkdirSync(path.join(__dirname, `../../../data`));
+}
 
 export const client = new Twitter(require('../../../.env.json'));
